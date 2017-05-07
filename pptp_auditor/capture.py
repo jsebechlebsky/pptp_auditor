1497ae8f56a4a8bd425a428a67a3f996fb6dc4eb
Jan Sebechlebsky <sebechlebskyjan@gmail.com>
2017-05-07 11:01:06 +0200

    capture.py cleanup

--- pptp_auditor/capture.py
+++ pptp_auditor/capture.py
@@ -3,17 +3,28 @@ from scapy.utils import PcapWriter
 import threading
 
 
-class PacketRecorder():
+class PacketRecorder:
+    """
+    Simple class providing pcap packet recording
+    running in separate thread
+    """
 
-    def __init__(self, target, file):
-        self.target = target
-        self.file = file
-        self._filter = 'host {0}'.format(target)
+    def __init__(self, _target, _file):
+        """
+        Initialize pcap packet recorder
+
+        :param _target: IP address of the target
+        :param _file: pcap file name
+        """
+        self.target = _target
+        self.file = _file
+        self._filter = 'host {0}'.format(_target)
         self._stop_event = threading.Event()
         self._recorder_thread = threading.Thread(target=self._recorder_thread_func)
         self._pcap_writer = PcapWriter(file)
 
     def start(self):
+        """Start recording"""
         self._recorder_thread.start()
 
     def _recorder_thread_func(self):
@@ -23,5 +34,6 @@ class PacketRecorder():
             self._pcap_writer.write(pkts)
 
     def stop(self):
+        """Stop recording"""
         self._stop_event.set()
         self._recorder_thread.join()
